a6af5e0f693a25005dc06bcb7ab2107f61a208f9
Alif024 <aleefrock12345@gmail.com>
2024-07-30 09:25:41 +0700

    add nav-toggle when scroll

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import Image from "next/image";
 import Logo from "@/assets/logo-main.svg";
-import BgMenu from "@/assets/bg-menu-nav.svg"
-import MenuBtn from "@/assets/menu-nav.svg"
+import BgMenu from "@/assets/bg-menu-nav.svg";
+import MenuBtn from "@/assets/menu-nav.svg";
 import { Poppins } from "next/font/google";
 
 const poppinsSemiBold = Poppins({ weight: "600", subsets: ["latin"] });
@@ -10,22 +10,24 @@ const poppinsExtraBold = Poppins({ weight: "800", subsets: ["latin"] });
 
 function Navbar() {
   return (
-    <div className="navbar">
-      <div className="logo-img">
-        <Image src={Logo} fill alt="Logo" />
+    <>
+      <div className="navbar">
+        <div className="logo-img">
+          <Image src={Logo} fill alt="Logo" />
+        </div>
+        <div className={`${poppinsSemiBold.className} nav-btns`}>
+          <p className={`${poppinsExtraBold.className} active`}>Home</p>
+          <p>Products</p>
+          <p>Order</p>
+          <p>Payment</p>
+          <p>Account</p>
+        </div>
       </div>
       <div className="nav-mobile">
         <Image src={BgMenu} fill alt="bg menu" className="bg-menu-nav" />
         <Image src={MenuBtn} fill alt="bg menu" className="menu-nav-btn" />
       </div>
-      <div className={`${poppinsSemiBold.className} nav-btns`}>
-        <p className={`${poppinsExtraBold.className} active`}>Home</p>
-        <p>Products</p>
-        <p>Order</p>
-        <p>Payment</p>
-        <p>Account</p>
-      </div>
-    </div>
+    </>
   );
 }
 
